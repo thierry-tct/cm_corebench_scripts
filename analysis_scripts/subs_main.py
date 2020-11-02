f9669c1a326b97236e8fde6895a7c1cc6a0850e5
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-11-02 19:17:16 +0100

    Update subs_main.py

--- analysis_scripts/subs_main.py
+++ analysis_scripts/subs_main.py
@@ -302,7 +302,7 @@ def main():
             plot.plt.savefig(image_file+".pdf", format='pdf') #, bbox_extra_artists=(lgd,), bbox_inches='tight')
             plot.plt.close('all')
     
-            pivot_df = data_df.pivot(columns='Techs', values="Subsuming MS")
+            pivot_df = data_df.pivot(columns='Tech', values="Subsuming MS")
             order = ["PREDICTED", "RANDOM"]
             plot.plotBoxes({c: list(pivot_df[c]) for c in order}, order, image_file_agg, plot.colors_bw, ylabel="Subsuming MS", yticks_range=plot.np.arange(0,1.01,0.2))
     print("@DONE!")
