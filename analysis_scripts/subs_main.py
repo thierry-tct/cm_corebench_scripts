2e85d484849f316fe873a4f7b72ef769e7c076c9
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-12-23 22:25:11 +0100

    Update subs_main.py

--- analysis_scripts/subs_main.py
+++ analysis_scripts/subs_main.py
@@ -146,14 +146,15 @@ def load_data(in_top_dir, model_in_dir, cache=True):
     else:
         #cache_projs = set()
         all_tests = {}
-        all_mutants = {}
-        machine_translation_mutants = {}
-        decision_trees_mutants = {}
         mutants_to_killingtests = {}
         tests_to_killed_mutants = {}
-        tests_to_killed_subs_cluster = {}
-        mutant_to_subs_cluster = {}
-        subs_cluster_to_mutants = {}
+	
+    all_mutants = {}
+    machine_translation_mutants = {}
+    decision_trees_mutants = {}
+    tests_to_killed_subs_cluster = {}
+    mutant_to_subs_cluster = {}
+    subs_cluster_to_mutants = {}
 
     machine_translation_muts_json = os.path.join(model_in_dir, "predicted_mutants.json")
     all_muts_json = os.path.join(model_in_dir, "all_mutants.json")
@@ -232,7 +233,7 @@ def load_data(in_top_dir, model_in_dir, cache=True):
                     tests_to_killed_subs_cluster[pname][t].add(mutant_to_subs_cluster[pname][km])
 
     if update_cache:
-        subs_load.common_fs.dumpJSON([all_tests, fault_tests, relevant_mutants_to_relevant_tests, mutants_to_killingtests, tests_to_killed_mutants], cache_file)
+        subs_load.common_fs.dumpJSON([all_tests, mutants_to_killingtests, tests_to_killed_mutants], cache_file)
         print ("# Cache Written, with {} projects!".format(len(all_tests)))
         
     return all_tests, all_mutants, machine_translation_mutants, decision_trees_mutants, mutants_to_killingtests, \
